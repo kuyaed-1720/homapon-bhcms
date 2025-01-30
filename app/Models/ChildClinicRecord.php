6ed3be2612d98b7249dd27b4afa121c1c4a6047f
atelier-eureka <ateliereureka1720@gmail.com>
2025-01-30 17:03:12 +0800

    Edit childclinicrecord model and schema

--- app/Models/ChildClinicRecord.php
+++ app/Models/ChildClinicRecord.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChildClinicRecord extends Model
 {
-    //
+    protected $fillable = [
+        'child_id',
+        'epi_status',
+        'bcg',
+        'dpt',
+        'opv',
+        'hepa_b',
+        'am',
+        'type_of_feeding',
+        'wt',
+        'vs',
+        'chief_complaint',
+        'other_cc',
+        'management',
+        'record_date'
+    ];
 }
--- database/migrations/2025_01_30_081032_create_child_clinic_records_table.php
+++ database/migrations/2025_01_30_081032_create_child_clinic_records_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('child_clinic_records', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('child_id')->constrained('children');
+            $table->enum('epi_status', ['complete', 'incomplete'])->nullable();
+            $table->enum('type_of_feeding', ['bf', 'mixed;', 'bot'])->nullable();
+            $table->boolean('bcg')->default(false);
+            $table->boolean('dpt')->default(false);
+            $table->boolean('opv')->default(false);
+            $table->boolean('hepa_b')->default(false);
+            $table->boolean('am')->default(false);
+            $table->string('wt');
+            $table->string('vs');
+            $table->text('chief_complaint');
+            $table->text('other_cc')->nullable();
+            $table->string('management');
+            $table->date('record_date');
             $table->timestamps();
         });
     }
