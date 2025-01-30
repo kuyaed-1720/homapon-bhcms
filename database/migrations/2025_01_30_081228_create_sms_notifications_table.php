b936455563b7c7b52fdfcffa1065ec955c5daa93
atelier-eureka <ateliereureka1720@gmail.com>
2025-01-30 22:16:38 +0800

    Edit smsnotification model and schema

--- database/migrations/2025_01_30_081228_create_sms_notifications_table.php
+++ database/migrations/2025_01_30_081228_create_sms_notifications_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sms_notifications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->text('message');
+            $table->timestamp('sent_at');
             $table->timestamps();
         });
     }
