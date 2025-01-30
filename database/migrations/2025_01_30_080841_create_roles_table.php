8d3601598aac7eac6faed2e0356064b52b9dd131
atelier-eureka <ateliereureka1720@gmail.com>
2025-01-30 16:33:40 +0800

    Edit role model and schema

--- database/migrations/2025_01_30_080841_create_roles_table.php
+++ database/migrations/2025_01_30_080841_create_roles_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->enum('role_name', ['admin', 'bhw', 'resident']);
             $table->timestamps();
         });
     }
