d334bce4744b3df6964e3424a3c35afb809d1bc4
atelier-eureka <ateliereureka1720@gmail.com>
2025-01-30 17:29:20 +0800

    Edit healthrecord model and schema

--- app/Models/HealthRecord.php
+++ app/Models/HealthRecord.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class HealthRecord extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'ph_member',
+        'philhealth_no',
+        'height',
+        'weight',
+        'bmi',
+        'waist_circumference',
+        'with_hpn',
+        'with_dm',
+        'pregnant',
+        'lmp',
+        'pregnancy_test',
+        'gp',
+        'smoking',
+        'alcohol_intake',
+        'had_five_drinks',
+        'chief_complaint',
+        'diagnosis',
+        'management',
+        'record_date',
+    ];
 }
--- database/migrations/2025_01_30_080939_create_health_records_table.php
+++ database/migrations/2025_01_30_080939_create_health_records_table.php
@@ -13,6 +13,26 @@ return new class extends Migration
     {
         Schema::create('health_records', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->boolean('ph_member')->default(false);
+            $table->string('philhealth_no')->nullable();
+            $table->integer('height')->nullable();
+            $table->integer('weight')->nullable();
+            $table->integer('bmi')->nullable();
+            $table->integer('waist_circumference')->nullable();
+            $table->enum('with_hpn', ['none', 'old', 'new'])->default('none');
+            $table->enum('with_dm', ['none', 'old', 'new'])->default('none');
+            $table->boolean('pregnant')->default(false);
+            $table->date('lmp')->nullable();
+            $table->string('pregnancy_test')->nullable();
+            $table->string('gp')->nullable();
+            $table->enum('smoking', ['never', 'current', 'passive', 'recently_stopped', 'long_stopped'])->default('never');
+            $table->boolean('alcohol_intake')->default(false);
+            $table->boolean('had_five_drinks')->default(false);
+            $table->text('chief_complaint')->nunllable();
+            $table->text('diagnosis')->nullable();
+            $table->string('management')->nullable();
+            $table->date('record_date');
             $table->timestamps();
         });
     }
